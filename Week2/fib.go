189f56767fbf38276081947bf75c75423c75a983
Jezrien Hsieh <jtr860830@yahoo.com.tw>
2018-04-18 20:25:40 +0800

    Add some tips to fib.go

--- Week2/fib.go
+++ Week2/fib.go
@@ -9,17 +9,17 @@ func main() {
 	fmt.Println("Recursive")
 	for i := 8; i <= 40; i += 8 {
 		start := time.Now()
-		fibRec(i)
+		value := fibRec(i)
 		elapsed := time.Since(start)
-		fmt.Printf("Fib(%d) ans: %d, time: %s\n", i, fibRec(i), elapsed)
+		fmt.Printf("Fib(%d) ans: %d, time: %s\n", i, value, elapsed)
 	}
 
 	fmt.Println("\nLoop")
 	for i := 8; i <= 40; i += 8 {
 		start := time.Now()
-		fibLoop(i)
+		value := fibLoop(i)
 		elapsed := time.Since(start)
-		fmt.Printf("Fib(%d) ans: %d, time: %s\n", i, fibLoop(i), elapsed)
+		fmt.Printf("Fib(%d) ans: %d, time: %s\n", i, value, elapsed)
 	}
 }
 
@@ -31,14 +31,14 @@ func fibRec(n int) int {
 }
 
 func fibLoop(n int) int {
-	f := make([]int, n+1, n+2)
+	f := make([]int, n+1, n+2) //Create an array size is n+1 but can be n+2
 	if n < 2 {
 		f = f[0:2]
 	}
 	f[0] = 0
-	f[1] = 1
+	f[1] = 1 //Fib(0) must be 0, Fib(1) must be 1
 	for i := 2; i <= n; i++ {
-		f[i] = f[i-1] + f[i-2]
+		f[i] = f[i-1] + f[i-2] // Fib(n) = Fib(n-1) + Fib(n-2)
 	}
 	return f[n]
 }
